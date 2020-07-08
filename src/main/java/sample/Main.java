75efedca8bf459fc5556bd3376545222a5f6d37a
luke newton <lukeantonynewton@gmail.com>
2020-07-08 09:31:07 -0400

    add main class and fix pom

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,20 +2,24 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lombok.val;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        val loader = new FXMLLoader(
+                Main.class.getResource("/SearchController.fxml"));
+        val page = loader.load();
+        val scene = new Scene((AnchorPane) page);
+
+        primaryStage.setTitle("Title goes here");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
